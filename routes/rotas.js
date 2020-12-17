d6f150eec320157e96798c9efa5aaf5b3cb16843
Jhonathan Rasmussen <jhonathan.rasmussen@trevilub.com.br>
2020-12-17 16:20:29 -0300

    tudo certo

--- routes/rotas.js
+++ routes/rotas.js
@@ -2,16 +2,12 @@ const express = require('express');
 const router  = express.Router();
 const api     = require('../controllers/api');
 const convert = require('xml-js');
+const xml2js = require('xml2js');
 
 router.post('/limite_positivo', async function (req, res, next){
-    res.send({response: resultado = convert.xml2json(await api.limite_positivo(req), {compact: true, spaces: 4})});
-
-    //var result = convert.xml2json(xml, {compact: true, spaces: 4});
-
-    console.log(resultado);
-
-    return resultado;
-});
+    let rps = JSON.parse(convert.xml2json(await api.limite_positivo(req), {compact: true, spaces: 4}));
+    res.send(rps);
+    });
 
 router.post('/negocio_positivo', (req, res, next) =>{
 });
